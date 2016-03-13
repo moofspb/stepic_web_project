c7fdc05f0098a91bc2878c2571cbe4247a57a0f8
Mikhail Kuntsevich <moofspb@gmail.com>
2016-03-13 21:44:31 +0300

    creating ask project

---
+++ ask/ask/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'qa.views.test')
+    url(r'^login/', 'qa.views.test')
+    url(r'^signup/', 'qa.views.test')
+    url(r'^question/\d+/', 'qa.views.test')
+    url(r'^ask/', 'qa.views.test')
+    url(r'^popular/', 'qa.views.test')
+    url(r'^new/', 'qa.view.test')
+    # url(r'^$', 'ask.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
