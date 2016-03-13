34444e816e9bb82f057cee5859d0cf5ad668bdd4
Mikhail Kuntsevich <moofspb@gmail.com>
2016-03-13 22:25:08 +0300

    editing urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'qa.views.test')
-    url(r'^login/', 'qa.views.test')
-    url(r'^signup/', 'qa.views.test')
-    url(r'^question/\d+/', 'qa.views.test')
-    url(r'^ask/', 'qa.views.test')
-    url(r'^popular/', 'qa.views.test')
-    url(r'^new/', 'qa.view.test')
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/', 'qa.views.test'),
+    url(r'^signup/', 'qa.views.test'),
+    url(r'^question/\d+/', 'qa.views.test'),
+    url(r'^ask/', 'qa.views.test'),
+    url(r'^popular/', 'qa.views.test'),
+    url(r'^new/', 'qa.view.test'),
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
