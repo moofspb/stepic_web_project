b38e45d3b8cbdc6e10def4cd88e037ab0621491a
Mikhail Kuntsevich <moofspb@gmail.com>
2016-03-14 16:54:35 +0300

    editing urlspatterns in urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from qa import views
 
-urlpatterns = [
-    ulr(r'^$', 'test'),
+urlpatterns = patterns('qa.views',
+    url(r'^$', 'test'),
     url(r'^login/.*$', 'test', name='login'),
     url(r'^signup/.*', 'test', name='signup'),
     url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
     url(r'^ask/.*', 'test', name='ask'),
     url(r'^popular/.*', 'test', name='popular'),
     url(r'^new/.*', 'test', name='new'),
-]
+)
