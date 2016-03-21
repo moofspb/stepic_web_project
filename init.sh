e44a8ed2caa71daa31a60926de54d17e0b4413ff
Mikhail Kuntsevich <moofspb@gmail.com>
2016-03-21 14:52:13 +0300

    changing configs

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo rm -r /etc/gunicorn.d/*
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/qa.py  /etc/gunicorn.d/qa.py
-sudo /etc/init.d.gunicorn restart
+sudo /bin/ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo /bin/ln -sf /home/box/web/etc/qa.py  /etc/gunicorn.d/qa.py
+sudo /etc/init.d/gunicorn restart
